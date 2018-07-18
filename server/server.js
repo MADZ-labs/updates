1d38d79bb5e704ee20c7de799255eb2725884eae
Zach Coursey <zcoursey22@gmail.com>
2018-07-18 14:33:01 -0700

    Server set up and serves html

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3004;
+
+app.use(express.static('client/dist'));
+
+app.listen(port, console.log(`server running on port ${port}`));
