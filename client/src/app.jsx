b0334cc9d735bd3ed79ac59b35ef1173d7295a7b
Zach Coursey <zcoursey22@gmail.com>
2018-07-18 19:28:46 -0700

    Create app.jsx for entry

---
+++ client/src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return <span>Hello React</span>
+  }
+};
+
+ReactDOM.render(<App />, document.querySelector('#app'));
