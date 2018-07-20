9d11f02e802ee9b879a731f800189a276cdd07d4
Zach Coursey <zcoursey22@gmail.com>
2018-07-19 19:51:03 -0700

    Refactor components to handle how date is formatted

--- client/src/components/update.jsx
+++ client/src/components/update.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const Update = (props) => {
+  const date = props.update.date;
+  const formattedDate = `${monthNumberToWord(date.getMonth())} ${date.getDate()}, ${date.getFullYear()}`;
   return (
     <div>
-      <p>{props.update.date}</p>
+      <hr></hr>
+      <p>{formattedDate}</p>
       <h3>{props.update.title}</h3>
       <p>{props.update.description}</p>
       <span>{props.update.comments} comments</span>
@@ -15,3 +18,31 @@ const Update = (props) => {
 };
 
 module.exports = Update;
+
+const monthNumberToWord = (num) => {
+  if (num === 0) {
+    return 'January'
+  } else if (num === 1) {
+    return 'February'
+  } else if (num === 2) {
+    return 'March'
+  } else if (num === 3) {
+    return 'April'
+  } else if (num === 4) {
+    return 'May'
+  } else if (num === 5) {
+    return 'June'
+  } else if (num === 6) {
+    return 'July'
+  } else if (num === 7) {
+    return 'August'
+  } else if (num === 8) {
+    return 'September'
+  } else if (num === 9) {
+    return 'October'
+  } else if (num === 10) {
+    return 'November'
+  } else {
+    return 'December'
+  };
+};
