8d5f030deaf100a1269439c81a63bb730fc3a41d
Zach Coursey <zcoursey22@gmail.com>
2018-07-19 19:46:18 -0700

    Change how dates are handled

--- sampleData.js
+++ sampleData.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
-    dateCreated: 'June 11, 2018',
-    endingDate: 'July 11, 2018',
+    dateCreated: new Date('June 11, 2018'),
+    endingDate: new Date('July 11, 2018'),
     backers: '253',
     moneyRaised: '$1,537',
     goal: '$1,200',
@@ -9,7 +9,7 @@ module.exports = [
       {
         title: 'Halfway there!',
         description: 'In only four days, we\'ve already recieved over half of our goal of $1,200! Thank you so much to every single backer. Stay tuned for more updates.',
-        date: 'June 15, 2018',
+        date: new Date('June 15, 2018'),
         comments: 0,
         likes: 3,
         backersOnly: false,
@@ -17,7 +17,7 @@ module.exports = [
       {
         title: 'Great news for backers!',
         description: 'This is a secret update only to be seen by backers!',
-        date: 'June 28, 2018',
+        date: new Date('June 28, 2018'),
         comments: 4,
         likes: 2,
         backersOnly: true,
@@ -25,7 +25,7 @@ module.exports = [
       {
         title: '100% funded!',
         description: 'Congrats to each and every one of you! We have exceeded our goal for our funding time. Soon each backer will recieve our product!',
-        date: 'July 11, 2018',
+        date: new Date('July 11, 2018'),
         comments: 1,
         likes: 7,
         backersOnly: false,
