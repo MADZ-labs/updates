4f2b177a6f8bba61df741468ad9252a970781461
Zach Coursey <zcoursey22@gmail.com>
2018-07-19 17:28:30 -0700

    Attach sample data to entry component and add the first data entry to state as the current project

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,10 +1,20 @@
 import './styles/main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
+const sampleData = require('../../sampleData.js');
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      project: sampleData[0],
+    };
+  }
+
   render() {
-    return <span>Hello React</span>
+    return  (
+      <div></div>
+    )
   }
 };
 
