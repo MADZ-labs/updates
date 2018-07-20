79e9fa4b7014fc8b37a5ed6fc0403ee30b733e85
Zach Coursey <zcoursey22@gmail.com>
2018-07-19 17:24:28 -0700

    Create a file to store sample data

---
+++ sampleData.js
@@ -0,0 +1,35 @@
+module.exports = [
+  {
+    dateCreated: 'June 11, 2018',
+    endingDate: 'July 11, 2018',
+    backers: '253',
+    moneyRaised: '$1,537',
+    goal: '$1,200',
+    updates: [
+      {
+        title: 'Halfway there!',
+        description: 'In only four days, we\'ve already recieved over half of our goal of $1,200! Thank you so much to every single backer. Stay tuned for more updates.',
+        date: 'June 15, 2018',
+        comments: 0,
+        likes: 3,
+        backersOnly: false,
+      },
+      {
+        title: 'Great news for backers!',
+        description: 'This is a secret update only to be seen by backers!',
+        date: 'June 28, 2018',
+        comments: 4,
+        likes: 2,
+        backersOnly: true,
+      },
+      {
+        title: '100% funded!',
+        description: 'Congrats to each and every one of you! We have exceeded our goal for our funding time. Soon each backer will recieve our product!',
+        date: 'July 11, 2018',
+        comments: 1,
+        likes: 7,
+        backersOnly: false,
+      },
+    ],
+  },
+];
