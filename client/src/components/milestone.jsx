67912200f39c14161168791238e3db99b60d80da
Zach Coursey <zcoursey22@gmail.com>
2018-07-26 17:51:45 -0700

    Refactor milestone component to render both types of milestones

--- client/src/components/milestone.jsx
+++ client/src/components/milestone.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import helpers from '../../../helpers/helpers.js';
 
 const Milestone = (props) => {
-  const date = props.project.dateCreated;
+  let date = null;
+  let message = null;
+  if (!props.update) {
+    date = props.project.dateCreated;
+    message = 'Project launched';
+  } else {
+    date = props.project.endingDate;
+    message = `Successfully raised ${props.project.moneyRaised} with ${props.project.backers} backers`;
+  };
   const formattedDate = `${helpers.monthNumberToString(date.getMonth())} ${date.getDate()}, ${date.getFullYear()}`;
   return (
     <div>
       <span>{formattedDate}</span>
-      <h3>Project launched</h3>
+      <h3>{message}</h3>
     </div>
   )
 };
