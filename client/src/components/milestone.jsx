5aaafa395bb28f0134d23ddf2f16fb488822dbcf
Zach Coursey <zcoursey22@gmail.com>
2018-07-26 18:52:03 -0700

    Milestone passes linter

--- client/src/components/milestone.jsx
+++ client/src/components/milestone.jsx
@@ -1,23 +1,51 @@
 import React from 'react';
-import helpers from '../../../helpers/helpers.js';
+import PropTypes from 'prop-types';
+import helpers from '../../../helpers/helpers';
 
-const Milestone = (props) => {
+const Milestone = ({ update, project }) => {
   let date = null;
   let message = null;
-  if (!props.update) {
-    date = props.project.dateCreated;
+  if (!update) {
+    date = project.dateCreated;
     message = 'Project launched';
   } else {
-    date = props.project.endingDate;
-    message = `Successfully raised ${props.project.moneyRaised} with ${props.project.backers} backers`;
-  };
+    date = project.endingDate;
+    message = `Successfully raised ${project.moneyRaised} with ${project.backers} backers`;
+  }
   const formattedDate = `${helpers.monthNumberToString(date.getMonth())} ${date.getDate()}, ${date.getFullYear()}`;
   return (
     <div>
-      <span>{formattedDate}</span>
-      <h3>{message}</h3>
+      <span>
+        {formattedDate}
+      </span>
+      <h3>
+        {message}
+      </h3>
     </div>
-  )
+  );
+};
+
+Milestone.propTypes = {
+  update: PropTypes.element,
+  project: PropTypes.shape({
+    dateCreated: PropTypes.instanceOf(Date),
+    endingDate: PropTypes.instanceOf(Date),
+    backers: PropTypes.string,
+    moneyRaised: PropTypes.string,
+    goal: PropTypes.string,
+    updates: PropTypes.arrayOf(PropTypes.shape({
+      title: PropTypes.string,
+      description: PropTypes.string,
+      date: PropTypes.instanceOf(Date),
+      comments: PropTypes.number,
+      likes: PropTypes.number,
+      backersOnly: PropTypes.boolean,
+    })),
+  }).isRequired,
+};
+
+Milestone.defaultProps = {
+  update: null,
 };
 
 module.exports = Milestone;
