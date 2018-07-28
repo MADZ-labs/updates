3e1f154b827f3995949b74d0e724aafefbe618c6
Zach Coursey <zcoursey22@gmail.com>
2018-07-27 17:47:26 -0700

    Makes milestone a styled component

--- client/src/components/milestone.jsx
+++ client/src/components/milestone.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import helpers from '../../../helpers/helpers';
 
 const Milestone = ({ update, project }) => {
+  const MilestoneDiv = styled.div`
+    margin: 50px auto;
+    padding: 50px 25px;
+    color: white;
+    background: ${!update ? '#034752' : '#009E74'}
+    width: 550px;
+    text-align: center;
+    font-family: sans-serif;
+    > span {
+      font-size: 14px;
+    }
+    > h3 {
+      margin: 0;
+      margin-top: ${!update ? '5px' : '20px'};
+      padding: 0;
+      font-family: ${!update ? 'EB Garamond' : 'sans-serif'};
+      font-size: ${!update ? '36px' : '22px'};
+      font-weight: normal;
+    }
+  `;
   let date = null;
   let message = null;
   if (!update) {
@@ -14,19 +35,26 @@ const Milestone = ({ update, project }) => {
   }
   const formattedDate = `${helpers.monthNumberToString(date.getMonth())} ${date.getDate()}, ${date.getFullYear()}`;
   return (
-    <div>
+    <MilestoneDiv>
       <span>
         {formattedDate}
       </span>
       <h3>
         {message}
       </h3>
-    </div>
+    </MilestoneDiv>
   );
 };
 
 Milestone.propTypes = {
-  update: PropTypes.element,
+  update: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.string,
+    date: PropTypes.instanceOf(Date),
+    comments: PropTypes.number,
+    likes: PropTypes.number,
+    backersOnly: PropTypes.bool,
+  }),
   project: PropTypes.shape({
     dateCreated: PropTypes.instanceOf(Date),
     endingDate: PropTypes.instanceOf(Date),
