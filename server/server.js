976d8c5a7b22c2905628a3ad268733396644f1fa
Zach Coursey <zcoursey22@gmail.com>
2018-07-29 13:46:09 -0700

    Sets up route to grab project with updates from db

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const Log = require('log');
 
+const db = require('../db/db');
+
 const log = new Log();
 
 const app = express();
@@ -9,8 +11,14 @@ const port = process.env.PORT || 3004;
 
 app.use(express.static('client/dist'));
 
-app.get('/updates', (req, res) => {
-  res.send('get updates');
+app.get('/projects/:id', (req, res) => {
+  db.query(`SELECT backers,date_created,ending_date,goal,money_raised FROM projects WHERE id=${req.params.id}`, (error, results) => {
+    const project = results[0];
+    db.query(`SELECT backers_only,comments,description,likes,title,update_date FROM updates WHERE project_id=${req.params.id}`, (updatesError, updatesResults) => {
+      project.updates = updatesResults;
+      res.send(project);
+    });
+  });
 });
 
 if (process.env.NODE_ENV !== 'test') {
