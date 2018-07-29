a073a3e7381fb60e5f6f17f9cc81ded94d65b762
Zach Coursey <zcoursey22@gmail.com>
2018-07-29 15:12:29 -0700

    Changes sample data to reflect actual data from db

--- sampleData.js
+++ sampleData.js
@@ -2,14 +2,14 @@ module.exports = [
   {
     dateCreated: new Date('June 11, 2018'),
     endingDate: new Date('July 11, 2018'),
-    backers: '253',
-    moneyRaised: '$1,537',
-    goal: '$1,200',
+    backers: 253,
+    moneyRaised: 1537,
+    goal: 1200,
     updates: [
       {
         title: 'Halfway there!',
         description: 'In only four days, we\'ve already recieved over half of our goal of $1,200! Thank you so much to every single backer. Stay tuned for more updates.',
-        date: new Date('June 15, 2018'),
+        updateDate: new Date('June 15, 2018'),
         comments: 0,
         likes: 3,
         backersOnly: false,
@@ -17,7 +17,7 @@ module.exports = [
       {
         title: 'Great news for backers!',
         description: 'This is a secret update only to be seen by backers!',
-        date: new Date('June 28, 2018'),
+        updateDate: new Date('June 28, 2018'),
         comments: 4,
         likes: 2,
         backersOnly: true,
@@ -25,7 +25,7 @@ module.exports = [
       {
         title: '100% funded!',
         description: 'Congrats to each and every one of you! We have exceeded our goal for our funding time. Soon each backer will recieve our product!',
-        date: new Date('July 11, 2018'),
+        updateDate: new Date('July 11, 2018'),
         comments: 1,
         likes: 7,
         backersOnly: false,
@@ -33,7 +33,7 @@ module.exports = [
       {
         title: 'Thanks!',
         description: 'We just wanted to reach out and say thanks once again to everyone! We did it!',
-        date: new Date('July 14, 2018'),
+        updateDate: new Date('July 14, 2018'),
         comments: 2,
         likes: 2,
         backersOnly: false,
