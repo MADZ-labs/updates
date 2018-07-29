618d0f7c3c000e49020d1dc64209269603288290
Zach Coursey <zcoursey22@gmail.com>
2018-07-29 15:42:04 -0700

    Configures circle ci to work with mysql and change server test for new route

--- tests/server.test.js
+++ tests/server.test.js
@@ -2,19 +2,14 @@ const request = require('supertest');
 
 const app = require('../server/server.js');
 
-describe('get /updates', () => {
+describe('get /projects/:id', () => {
   it('should return a status code of 200 for a valid request', async () => {
-    const response = await request(app).get('/updates');
+    const response = await request(app).get('/projects/25');
     expect(response.statusCode).toBe(200);
   });
 
-  it('should return "get updates" for a valid request', async () => {
-    const response = await request(app).get('/updates');
-    expect(response.text).toBe('get updates');
-  });
-
   it('should return a status code of 404 for a bad request', async () => {
-    const response = await request(app).get('/update');
+    const response = await request(app).get('/project');
     expect(response.statusCode).toBe(404);
   });
 });
