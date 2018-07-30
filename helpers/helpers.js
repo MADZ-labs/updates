43f5a2a18b1bb4eed99cc552ee90ab3a8db1625b
Zach Coursey <zcoursey22@gmail.com>
2018-07-29 19:37:35 -0700

    Removes useless helper function and tests for it

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,21 +1,3 @@
-const monthNumberToString = (num, short = false) => {
-  const months = {
-    0: 'January',
-    1: 'February',
-    2: 'March',
-    3: 'April',
-    4: 'May',
-    5: 'June',
-    6: 'July',
-    7: 'August',
-    8: 'September',
-    9: 'October',
-    10: 'November',
-    11: 'December',
-  };
-  return short ? months[num].substring(0, 3) : months[num];
-};
-
 const cutOffDesc = (desc) => {
   const words = desc.split(' ');
   if (words.length > 24) return `${words.slice(0, 24).join(' ')}... `;
@@ -38,6 +20,5 @@ const getColor = (newRender) => {
   return '#fca';
 };
 
-module.exports.monthNumberToString = monthNumberToString;
 module.exports.cutOffDesc = cutOffDesc;
 module.exports.getColor = getColor;
